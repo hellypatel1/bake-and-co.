a79425a7c7a5c045666c88240780ac2db6d02e67
hellypatel1 <hpp2@njit.edu>
2024-12-14 04:04:28 -0500

    added script for header hamgurger

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-console.log('Welcome to Bake & Co.!');
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+  
+    hamburger.addEventListener('click', () => {
+      mobileMenu.classList.toggle('open');
+      hamburger.classList.toggle('open'); // Toggle the X icon
+    });
+  
+    document.addEventListener('click', (event) => {
+      if (!mobileMenu.contains(event.target) && !hamburger.contains(event.target)) {
+        mobileMenu.classList.remove('open');
+        hamburger.classList.remove('open'); // Reset the X icon
+      }
+    });
+  });
+  
